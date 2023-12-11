178eb3c87e8587ccaeeadea606bb545e12a65abc
TanimShahriar <ssmtsshr@gmail.com>
2023-12-11 15:22:35 +0600

    job experience section completed

--- src/Pages/Home/Experience/Experience.jsx
+++ src/Pages/Home/Experience/Experience.jsx
@@ -6,14 +6,24 @@ const Experience = () => {
       <h2 className="font-bold text-4xl  border-b-4 w-80 text-center mx-auto opacity-70 mb-10">Job Experience</h2>
 
       {/* card */}
-      <div>
+      <div className="space-y-5 opacity-70 hover:opacity-90">
         {/* card-1 */}
-        <div>
-
+        <div className="hover:bg-[#723d9c] rounded-lg  p-2 border border-slate-500  h-auto  hover:shadow-lg hover:transform hover:scale-105 duration-500 ease-in-out">
+          <h2>Company name: Vcube Soft & Tech</h2>
+          <h2>Job position: QA Engineer</h2>
+          <h2>Job period: October, 2021- October, 2023</h2>
+          <h2>Location: Baridhara, Dhaka, Bangladesh</h2>
+          <h2>Job Type: Remote</h2>
         </div>
 
         {/* card-2 */}
-        <div></div>
+        <div className="hover:bg-[#723d9c] rounded-lg  p-2 border border-slate-500  h-auto  hover:shadow-lg hover:transform hover:scale-105 duration-500 ease-in-out">
+          <h2>Company name: Bismillah Chemical</h2>
+          <h2>Job position: Computer admin</h2>
+          <h2>Job period: June, 2020- August, 2023</h2>
+          <h2>Location: Uttara, sector-4, Dhaka, Bangladesh</h2>
+          <h2>Job Type: Onsite</h2>
+        </div>
       </div>
     </div>
   );
